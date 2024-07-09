890cc8498ce25e051f1d82cbef79abccde0fc709
HereToLearnhaha <itsgaurab4@gmail.com>
2024-07-09 13:32:12 +0545

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // WAP to find the sum of 2 numbers
+//        System.out.println("Enter 2 numbers");
+//        Scanner myObj = new Scanner(System.in);
+//        int num1 = myObj.nextInt();
+//        int num2 = myObj.nextInt();
+//        int sum = num1+num2;
+//        System.out.println("The sum is:" + sum);
+//
+//        // WAP to find area of a circle
+//        System.out.println("Enter radius");
+//        double r = myObj.nextDouble();
+//        double area = 3.14*r*r;
+//        System.out.println("The area is:" + area);
+//        myObj.close();
+
+//        // WAP to find simple interest
+        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter Principal: ");
+//        double Principal = input.nextDouble();
+//        System.out.println("Enter Rate: ");
+//        double Rate = input.nextDouble();
+//        System.out.println("Enter Time: ");
+//        double Time = input.nextDouble();
+//        double Simple_Interest = (Principal * Rate * Time)/100;
+//        System.out.println("Simple Interest: " + Simple_Interest);
+
+
+        // WAP to find if check odd or even
+        System.out.println("Enter a num");
+        int num1 = input.nextInt();
+        if(num1%2==0){
+            System.out.println(num1 + " is even");
+        }
+        else if(num1%2==1){
+            System.out.println(num1 + " is odd");
+        }
+        // Diff between int & float
+//        int is used to store integers which don't have decimals
+//        Float is used to store floating point values which are decimals
+//        int is 32bit whereas float is 64 bit
+
+
+        }
+    }
